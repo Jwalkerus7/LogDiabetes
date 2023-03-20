d285b75a6c02b899a534e769618845cb60409e14
jason walker <jwalkerus3@yahoo.com>
2023-03-20 18:51:56 -0500

    Started Controller for DiabetesLog

--- LogDiabetes/Controllers/LogDiabetesController.cs
+++ LogDiabetes/Controllers/LogDiabetesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LogDiabetes.EntityModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,14 @@ namespace LogDiabetes.Controllers
 {
     public class LogDiabetesController : Controller
     {
+       Diabetes_Log DL = new Diabetes_Log();
+
         // GET: LogDiabetes
         public ActionResult Index()
         {
             return View();
         }
+
+
     }
 }
