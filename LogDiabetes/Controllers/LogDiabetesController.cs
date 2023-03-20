6568af193fd81dcc63af779ebe695a117eba7114
jason walker <jwalkerus3@yahoo.com>
2023-03-20 18:47:57 -0500

    Added a VM model DiabetesLogVM

---
+++ LogDiabetes/Controllers/LogDiabetesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LogDiabetes.Controllers
+{
+    public class LogDiabetesController : Controller
+    {
+        // GET: LogDiabetes
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
